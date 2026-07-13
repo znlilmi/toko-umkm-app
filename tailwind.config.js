351cb1e49dd7241c4893496b228a7357fa4169f4
nal <mzainalilmi04@gmail.com>
2026-07-13 16:27:02 +0800

    style(theme): customize indigo palette to Shopee Orange globally

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,22 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                // Overwrite indigo dengan warna khas Shopee Orange
+                indigo: {
+                    50: '#ffeee8',
+                    100: '#ffd0c4',
+                    200: '#ffb09e',
+                    300: '#ff8a72',
+                    400: '#ff6245',
+                    500: '#f05335',
+                    600: '#ee4d2d', // Shopee Orange
+                    700: '#d44327',
+                    800: '#b0341c',
+                    900: '#8c2815',
+                    950: '#6b1c0d',
+                }
+            }
         },
     },
 
